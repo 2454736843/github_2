08b6b92a6608dac218e9655084fe5eb1f05611a2
lyz001 <yuanzhao_li@163.com>
2018-09-22 08:49:57 +0800

    this is the second change

--- src/main/java/test1/StringToInt.java
+++ src/main/java/test1/StringToInt.java
@@ -3,5 +3,6 @@ package test1;
 public class StringToInt {
     public static void main(String[] args) {
         System.out.println("this is string to int");
+        System.out.println("hhrrr");
     }
 }
